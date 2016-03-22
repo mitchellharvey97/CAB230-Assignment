3e2aa80c559916ee57df0ed777570aca0b42a0b1
Mitch <mitchellharvey97@gmail.com>
2016-03-22 17:59:15 +1000

    Working on Php

--- dynamic_site/index.php
+++ dynamic_site/index.php
@@ -1,30 +1,94 @@
 <?php
-require('local_config/db_password.php'); //Include the password file -- added as each dev environment will have different db details
 
 
 
-// Create connection
+
+
+
+
+// Create connection -- Using the credentials from external file
+require('local_config/db_password.php'); //Include the password file -- added as each dev environment will have different db details using git ignore files to prevent cloning
 $conn = new mysqli($databases['host'], $databases['username'], $databases['password'], $databases['database']);
 
 // Check connection
-if ($conn->connect_error) {
+if ($conn->connect_error) { //Display Connection error if any
     die(
 	"Connection failed: " . $conn->connect_error);
+}
+
+//Collect parameters
+$filter = $_GET["filter"];	
+$parameter = $_GET["par"];	
+
+
+
+
+
+
+
+//$stmt->close();
+//$conn->close();
+
+
+	function print_results($conn, $filter_type, $filter_value){
+	   if ($filter_type == 'suburb'){
+		   
+	   $sql = 'SELECT * FROM Wifi WHERE Wifi.Suburb like "%' . $filter_value . '%"';
+	   }
+	   else if ($filter_type == 'name'){
+	   	  
+	   $sql = "SELECT * FROM Wifi WHERE `Wifi`.`Wifi Hotspot Name` like '%" . $filter_value . "%'";
+
+	   }
+		  else { 
+	   $sql ='SELECT * FROM Wifi';
+	   
+	   }
+	   
+echo "<ul>";
+
+
+
+    foreach ($conn->query($sql) as $row) {
+        print "<li> Hotspot Nameddd : " . $row['Wifi Hotspot Name']. "</li>
+		";
+    }
+	
 }
 
 
 
 
+//Insert whatever Junk below to make up the page 
+?>
+<html>
+<head>
+<title>TEST PAGE?? </title>
+
+</head>
+
+<body>
+<?php
+echo "Filter Type: ". $filter;
+echo "<br>";
+echo "Parameter: ". $parameter;
+echo "<br>";
+
+//testSql($conn);
 
 
-testSql($conn);
+print_results($conn, $filter, $parameter);	
+	
+
+	
+?>
+
+</body>
+
+</html>
 
 
-
-//$stmt->close();
-$conn->close();
-
-
+<?php
 
 
 function testSql($conn) {
@@ -36,11 +100,6 @@ function testSql($conn) {
 }
 
 
-
-for ($i = 0; $i < 10; $i++){
-	echo "TESTING<br>";
-	
-}
-
-
+$stmt->close();
+$conn->close(); //Close SQL Connection
 ?>
