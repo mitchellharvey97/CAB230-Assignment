c6ef6a19b626688432d39099dc21afcc92a56302
Mitch H <mitchellharvey97@gmail.com>
2016-04-26 14:04:09 +1000

    Adding some more stuff

--- dynamic_site/common_files/database_api.php
+++ dynamic_site/common_files/database_api.php
@@ -1,4 +1,66 @@
 <?php
+//Collect Data Call and work out what to do with it
+
+
+//$recieved_data = json_decode($_POST['json']);
+
+
+$requested = $_GET["q"];
+
+
+require('../local_config/db_password.php'); //Include the password file -- added as each dev environment will have different db details using git ignore files to prevent cloning
+$data_table = $databases['data_table']; //Change to a straight variable for simplicity
+
+//Make a connection
+$conn = new mysqli($databases['host'], $databases['username'], $databases['password'], $databases['database']);
+
+// Check connection
+if ($conn->connect_error) { //Display Connection error if any
+    die(
+	"Connection failed: " . $conn->connect_error);
+}
+
+
+echo ("TESTING HERE");
+
+
+
+
+if ($requested == "all_names"){
+	echo ("HERE ARE YOUR NAMES");
+
+	get_search_list();
+	
+}
+
+
+function get_search_list(){
+	global $data_table;	
+	global $conn;
+
+		 
+	   $sql ='SELECT * FROM ' . $data_table;
+	 
+	 
+	 
+$return_array = []; //initalize array to return;
+    foreach ($conn->query($sql) as $row) {
+		array_push($return_array, $row);
+		print_r $row;
+		echo ("<br>");
+    }
+	return $return_array;	
+	
+}
+
+
+
+
+
+//$stmt->close();
+$conn->close(); //Close SQL Connection
+
+?>
 
 
 
--- dynamic_site/js/script.js
+++ dynamic_site/js/script.js
@@ -1,49 +1,94 @@
 window.onload = function() {
+test_database_connection()
 load_script();
 }
 
 
+
+
+//test_database_connection()
+
+function test_database_connection(){
+	
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+      console.log(xhttp.responseText);
+    }
+  };
+  xhttp.open("GET", "http://cab230.local/common_files/database_api.php?q=all_names", true);
+  xhttp.send();
+  
+  
+  
+	
+}
+
+
+
+
 var search_bar;
 
-
+var parent;
 
 var sample_options = ["Value1", "value2", "Value3", "value11", "A New One", "SomethingRandom", "This Time With Value in Middle"];
-
-console.log(sample_options[0]);
-
-
 
 
 function load_script(){
 search_bar = document.getElementById("search_value");
-
+    parent = document.getElementById("main_search");
 //Add the keyup event to get the value;
 search_bar.onkeyup = function() {user_input()};
 
 }
 
 function user_input(){
-	report_search_value();
+	//A slightly complex process of getting the text from a passed text box and returning the
+	//Top 4 before passing it onto the add suggestion function
+	cleanup_suggestions();
+	
+	if (search_bar.value){
+	add_suggestions(get_search_results(search_bar.value));
+	}
+}
+
+function cleanup_suggestions(){
+	var curr_suggestions = document.getElementsByClassName('suggestion_holder')
+		if (curr_suggestions[0]){
+			parent.removeChild(curr_suggestions[0]);
+	}	
+}
+
+function add_suggestions(top_suggestions){
+		console.log(top_suggestions);
+
+	
+	//Create Holder for suggestions
+	var suggestion_holder = document.createElement('span');
+	suggestion_holder.innerHTML = '<ul class ="suggestion"></li>';	
+	suggestion_holder.classList.add("suggestion_holder");
+	parent.insertBefore(suggestion_holder, search_bar.nextSibling)
+	
+
+	
+	for (x in top_suggestions){
+	add_suggestion_li(suggestion_holder, top_suggestions[x]);
+	}
+	
 	
 }
 
-
-function report_search_value(){
-
-
-	console.log("Value is: " + search_bar.value)
-	
-	
-	matching_results = get_search_results(search_bar.value)
-	
-	console.log(matching_results);
+function add_suggestion_li(parent, text){
+		var li = document.createElement("li");
+  li.appendChild(document.createTextNode(text));
+  parent.appendChild(li);
 	
 }
-
-
 
 function get_search_results(search_string){
 var matches = sample_options.filter( function(value){ return regex_contains_search(search_string, value )});
+ //Only return the first 4 (Stop page getting too long)
+ matches = matches.splice(0,4);
  return matches;		
 }
 
