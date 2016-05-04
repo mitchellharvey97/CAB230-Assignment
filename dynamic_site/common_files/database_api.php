62e05dbd8eec76ed047aaa0e3a0973c086025341
Mitch H <mitchellharvey97@gmail.com>
2016-05-04 13:52:57 +1000

    Last before lecture

--- dynamic_site/common_files/database_api.php
+++ dynamic_site/common_files/database_api.php
@@ -1,8 +1,57 @@
 <?php
-$requested = $_GET["q"]; //Get the type the user wanted
+$requested = $_GET["q"]; //Get the type of query
+
+if ($requested == "all_names"){
+	$sql = 'SELECT `Wifi Hotspot Name` FROM Wifi';
+	$results = sql_query($sql);
+}
+
+else if ($requested == "all_location_data"){
+	$sql = "SELECT * FROM Wifi";
+	$results = sql_query($sql);	
+}
+
+else if ($requested == 'wifi'){
+//Get the name of the hotspot to return
+$hotspot_name = $_GET['name'];
+$sql = "SELECT * FROM Wifi WHERE `Wifi Hotspot Name` = '" . $hotspot_name . "'";
+$results = sql_query($sql);
+}
+
+else if ($requested == "search"){
+	$search_type = $_GET['search_type'];
+	
+	if ($search_type == "name"){
+		
+		
+	}
+	else if ($search_type == "suburb"){
+		
+	}
+	else if ($search_type == "rating"){
+		
+		
+	}
+	
+	print ("ITS A QUERY");
+	
+}
 
 
-require('./local_config/db_password.php'); //Include the password file -- added as each dev environment will have different db details using git ignore files to prevent cloning
+//If the result variable has been set - display it, otherwise display an error code
+if ($results){
+echo json_encode($results);
+
+}
+else{
+	echo("Please provide arguments in the url **ADD HELP SCRIPT HERE MAYBE **");
+}
+
+
+
+//Main function to connect to the database, exucute query and return;
+function sql_query($query){
+	require('./local_config/db_password.php'); //Include the password file -- added as each dev environment will have different db details using git ignore files to prevent cloning
 $data_table = $databases['data_table']; //Change to a straight variable for simplicity
 
 //Make a connection
@@ -13,56 +62,6 @@ if ($conn->connect_error) { //Display Connection error if any
     die(
 	"Connection failed: " . $conn->connect_error);
 }
-
-
-if ($requested == "all_names"){
-	$sql = 'SELECT `Wifi Hotspot Name` FROM Wifi';
-	$results = sql_query($sql);
-echo json_encode($results);	
-
-}
-else if ($requested == "all_location_data"){
-//	print "All Location data comming right up, hang tight <br>";	
-	$sql = "SELECT * FROM Wifi";
-
-	
-	$results = sql_query($sql);	
-	
-	echo json_encode($results);
-	
-	
-}
-
-
-else if ($requested == 'wifi'){
-$hotspot_name = $_GET['name'];
-
-$sql = "SELECT * FROM Wifi WHERE `Wifi Hotspot Name` = '" . $hotspot_name . "'";
-
-echo $sql;
-
-
-//	$sql = "SELECT * FROM Wifi";
-$results = sql_query($sql);
-
-echo "Displaying results for $hotspot_name <br>";
-
-
-
-echo json_encode($results);
-	
-}
-
-
-
-else{
-	print("Please provide arguments in the url **ADD HELP SCRIPT HERE MAYBE **");
-	
-}
-
-
-function sql_query($query){
-			global $conn;
 			$result = $conn->query($query);
 			$result_data_store = array();
 			if ($result->num_rows > 0) {
@@ -75,14 +74,6 @@ function sql_query($query){
      return -1;
 }
 return $result_data_store;	
-}
-
-
-
 $conn->close(); //Close SQL Connection
-
+}
 ?>
-
-
-
-
